307330ccb2f8c0e422c3a2ff04dac17128375be8
Ujjawal K. Panchal <ujjawalpanchal32@gmail.com>
2020-12-15 00:42:40 +0530

    setup and documentation.

---
+++ Collector.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Title: Xray Dataset Collector.
+    
+Created on Thu Sep  3 22:52:47 2020
+
+@author: Ujjawal.K.Panchal, @Email: ujjawalpanchal32@gmail.com
+
+Copyright (C) Ujjawal K. Panchal - All Rights Reserved.
+Unauthorized copying of this file, via any medium is strictly prohibited.
+Proprietary and confidential.
+"""
+#TODO.
+if __name___ == "__main__":
+	print("TODO")
